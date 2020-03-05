efc90660afc8bbf52d693f71652a36fbf41d6acf
kenji6665 <61704836+kenji6665@users.noreply.github.com>
2020-03-05 17:49:41 +0100

    Dodaj pliki projektów.

---
+++ Pracadomowa1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pracadomowa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
